91e22f1341b1b017ff6961327d481bde94a26462
Tottadi sai sasank <saisasank2323@gmail.com>
2026-02-20 21:43:05 +0530

    in the case of foreign key first drop the foreig key then drop the column

--- student_management/database/migrations/2025_12_25_100426_create_admin_table.php
+++ student_management/database/migrations/2025_12_25_100426_create_admin_table.php
@@ -31,6 +31,9 @@ return new class extends Migration
         //now this will drop this column from admin table but not drop the whole table because we have some data in this table and if we drop the whole table then we will lose all the data in this table so we will drop only the column which we have added in this migration file
         Schema::table('admin', function (Blueprint $table) {
             $table->dropColumn(['name', 'token']);
+            // if it was a foreign key then we have to drop the foreign key first and then drop the column 
+             $table->dropForeign(['email']);
+             $table->dropColumn('email');
         });
         //Schema::dropIfExists('admin');
     }
